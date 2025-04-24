8b0bb0108618ee301866483a8ba64924e94305ab
Om Magdum <om.college.id@gmail.com>
2025-04-24 23:36:27 +0530

    added babel plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import babel from 'vite-plugin-babel';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), babel()],
 })
